bfcc3c9fe2a4931bb3ed54469e87005910127d7b
Taylorblairr <taylorblair1128@gmail.com>
2024-02-06 15:30:37 -0700

    working on checkout still

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,14 +8,16 @@ import {
   ListGroup,
   Button,
   Card,
-  ListGroupItem,
+  Form,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { listProductsDetails } from "../actions/productActions";
 
-function ProductScreen({ match }) {
+function ProductScreen({ match, history }) {
+  const [qty, setQty] = useState(1);
+
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
@@ -23,6 +25,10 @@ function ProductScreen({ match }) {
   useEffect(() => {
     dispatch(listProductsDetails(match.params.id));
   }, []);
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
 
   return (
     <div>
@@ -81,8 +87,35 @@ function ProductScreen({ match }) {
                   </Row>
                 </ListGroup.Item>
 
+                {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col xs='auto' className="my-1">
+                        <Form.Control
+                            as='select'
+                            value={qty}
+                            onChange={(e) => setQty(e.target.value)}
+                            >
+                              {
+
+                                  [...Array(product.countInStock).keys()].map((x) =>(
+                                    <option key={x+1} value = {x}>
+                                      {x + 1}
+                                    </option>
+                                  ))
+                              }
+
+
+                        </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
+                  onClick={addToCartHandler}
                     classname="btn-block"
                     disabled={product.countInStock === 0}
                     type="button"
@@ -96,64 +129,7 @@ function ProductScreen({ match }) {
         </Row>
       )}
 
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} Ratings`}
-                color={"#f8e825"}
-              />
-            </ListGroup.Item>
-
-            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-
-            <ListGroup.Item>description: {product.description}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-
-        <Col md={3}>
-          <Card>
-            <ListGroup varient="flush">
-              <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-
-              <ListGroup.Item>
-                <Row>
-                  <Col>Stock:</Col>
-                  <Col>
-                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-
-              <ListGroup.Item>
-                <Button
-                  classname="btn-block"
-                  disabled={product.countInStock === 0}
-                  type="button"
-                >
-                  Add To cart
-                </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+      
     </div>
   );
 }
