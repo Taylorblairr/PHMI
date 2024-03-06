c55bf80d701b364ba1ce37596010b3a9c60680f1
Taylorblairr <taylorblair1128@gmail.com>
2024-03-06 15:00:27 -0700

    starting styling

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -1,4 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Paginate from "../components/Paginate";
+import { listProducts } from "../actions/productActions";
+
 function LandingScreen() {
-  return <h1>Please Hate Me Inc.</h1>;
+  return (
+    <div>
+      <img class="image1" src="https://imgur.com/ekpcjFR.png" />
+      <img class="image2" src="https://imgur.com/clYC2AC.png" />
+
+      <h1>Please Hate Me Inc.</h1>
+    </div>
+  );
 }
 export default LandingScreen;
