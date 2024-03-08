526b46873d953b9c81e6f6da1ff29ee7c7c0094e
Taylorblairr <taylorblair1128@gmail.com>
2024-03-08 14:16:53 -0700

    more style

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -5,10 +5,8 @@ function LandingScreen() {
   return (
     <div>
       {/* <Navbar /> */}
-
-      <img class="image1" src="https://i.imgur.com/jZSpngh.jpeg" />
-
-      <h1>Please Hate Me Inc.</h1>
+      <h1 class="title">Please Hate Me Inc.</h1>
+      <img class="bg" src="https://i.imgur.com/jZSpngh.jpeg" />
     </div>
   );
 }
