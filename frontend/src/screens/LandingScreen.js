f0d8e03238069ef46fae607cb9841c22e56117be
Taylorblairr <taylorblair1128@gmail.com>
2024-03-13 15:08:30 -0600

    still working on landing page

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -5,6 +5,7 @@ function LandingScreen() {
   return (
     <body>
       <p class="box1"></p>
+
       <nav class="navbar">
         <ul class="link-selectors"></ul>
         <ul class="nav-selectors">
@@ -16,8 +17,8 @@ function LandingScreen() {
       <div>
         {/* <Navbar /> */}
 
-        <h1 class="title">Please Hate Me Inc.</h1>
         <img class="bg" src="https://i.imgur.com/jZSpngh.jpeg" />
+        <h1 class="word">Please Hate Me Inc.</h1> 
       </div>
     </body>
   );
